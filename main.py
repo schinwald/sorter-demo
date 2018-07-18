57bfbab5c43696c6d9320f4e9aa01d4e3805c191
James Schinwald <jamesschinwald@gmail.com>
2018-07-18 15:37:18 -0400

    init insertion

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ def createTable(salg):
     print(salg)
     print("   +-------------------------------------------------------------+\n")
 
-alg = ["Merge Sort", "Heap Sort"]
-falg = [sorter.merge, sorter.heap]
+alg = ["Merge Sort", "Heap Sort", "Insertion Sort"]
+falg = [sorter.merge, sorter.heap, sorter.insertion]
 salg = algToString(alg)
 array = randomList(10)
 
