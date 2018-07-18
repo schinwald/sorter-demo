c8d06edb4db01317de340d8317e872f3e740ff07
James Schinwald <jamesschinwald@gmail.com>
2018-07-18 15:27:11 -0400

    fixing consistency issues

--- sorter/merge.py
+++ sorter/merge.py
@@ -1,5 +1,3 @@
-# Merge Sort
-# 
 # Description:
 # 
 # 
@@ -7,44 +5,43 @@
 
 import numpy
 
-# creates
 def sort(array):
     if len(array) > 1:
         # split the arrays
-        combined = []
         middle = (int)((len(array) + 1) / 2)
         split = numpy.split(array, [middle, len(array)])
 
-        aSplit = sort(split[0])
-        bSplit = sort(split[1])
-        
+        array_a = sort(split[0].tolist())
+        array_b = sort(split[1].tolist())
+
         i = 0
         j = 0
+        k = 0
         # sort and combine each of the arrays
-        while i < len(aSplit) and j < len(bSplit):
-            if aSplit[i] < bSplit[j]:
-                combined.append(aSplit[i])
+        while i < len(array_a) and j < len(array_b):
+            if array_a[i] < array_b[j]:
+                array[k] = array_a[i]
                 i += 1
-            elif bSplit[j] < aSplit[i]:
-                combined.append(bSplit[j])
+            elif array_b[j] < array_a[i]:
+                array[k] = array_b[j]
                 j += 1
             else:
-                combined.append(aSplit[i])
-                combined.append(bSplit[j])
+                array[k] = array_a[i]
+                array[k] = array_b[j]
                 i += 1
                 j += 1
 
+            k += 1
+
         # append the rest of the array
-        if i < len(aSplit):
-            for x in range(i, len(aSplit)):
-                combined.append(aSplit[x])
-        elif j < len(bSplit):
-            for x in range(j, len(bSplit)):
-                combined.append(bSplit[x])
-        
-        return combined
-    else:
-        return array
+        if i < len(array_a):
+            for x in range(i, len(array_a)):
+                array[k+x-i] = array_a[x]
+        elif j < len(array_b):
+            for x in range(j, len(array_b)):
+                array[k+x-j] = array_b[x]
+    
+    return array
 
 def explain():
     print("\n   Merge Sort:\n")
