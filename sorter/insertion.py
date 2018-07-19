b966fe28787d4446d14e339f666599e2fd0888aa
James Schinwald <jamesschinwald@gmail.com>
2018-07-19 00:11:04 -0400

    adding insertion sort

--- sorter/insertion.py
+++ sorter/insertion.py
@@ -4,7 +4,17 @@
 # Author: James Schinwald
 
 def sort(array):
-    pass
+    for i in range(1, len(array)):
+        num = array[i]
+
+        j = i-1
+        while j >= 0 and num < array[j]:
+            array[j+1] = array[j]
+            j -= 1
+
+        array[j+1] = num
 
 def explain():
-    pass
+    print("\n   Insertion Sort:\n")
+    # print("      1. Divide the unsorted list into n sublists, each containing 1 element (a list of 1 element is considered sorted).")
+    # print("      2. Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist remaining. This will be the sorted list.\n")
