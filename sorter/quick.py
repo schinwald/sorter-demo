4e25b00d7f0659d0c22c080c29a97f849f0cd877
James Schinwald <jamesschinwald@gmail.com>
2018-07-23 14:46:17 -0400

    adding comments and description

--- sorter/quick.py
+++ sorter/quick.py
@@ -1,5 +1,7 @@
 # Description:
-# 
+# Partitions the array in 3 sections, one for the pivot, one for all the numbers smaller than the pivot, and one for all the numbers larger than the pivot.
+# Organizes the partitions in ascending order (ie. smallest, pivot, largest) and then recursively partitioning and organizing the smaller and larger sides until 
+# everything is fully sorted.
 # 
 # Author: James Schinwald
 
@@ -32,4 +34,12 @@ def swap(array, a, b):
     array[b] = temp
 
 def explain():
-    pass
+    print("\n   Quick Sort:\n")
+    print("      Partitions the array in 3 sections, one for the pivot, one for all the numbers smaller than the pivot, and one for all the numbers larger than the pivot.")
+    print("      Organizes the partitions in ascending order (ie. smallest, pivot, largest) and then recursively partitioning and organizing the smaller and larger sides until") 
+    print("      everything is fully sorted.\n")
+    print("         Steps:\n")
+    print("            1. Declare the last index as the pivot point.")
+    print("            2. Loop through the specified section of the array shifting all numbers smaller than the pivot to the left; subsequently, leaving all larger numbers on the right.")
+    print("            3. Change specified section of array to be the smaller half and repeat step 2. using recursion until there is nothing left.")
+    print("            3. Change specified section of array to be the larger half and repeat step 2. using recursion until there is nothing left.\n")
