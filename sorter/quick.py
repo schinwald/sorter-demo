4b2808305ac020ac8b00a478a2f515c441e32430
James Schinwald <jamesschinwald@gmail.com>
2018-07-23 01:36:21 -0400

    adding quick sort

--- sorter/quick.py
+++ sorter/quick.py
@@ -4,7 +4,32 @@
 # Author: James Schinwald
 
 def sort(array):
-    pass
+    partitioner(array, 0, len(array)-1)
+
+def partitioner(array, low, high):
+    mid = partition(array, low, high)
+
+    if mid != low:
+        partitioner(array, low, mid-1)
+
+    if mid != high:
+        partitioner(array, mid+1, high)
+
+def partition(array, low, high):
+    pivot = array[high]
+    
+    j = low
+    for i in range(low, high+1):
+        if array[i] <= pivot:
+            swap(array, i, j)
+            j += 1
+
+    return j-1
+        
+def swap(array, a, b):
+    temp = array[a]
+    array[a] = array[b]
+    array[b] = temp
 
 def explain():
     pass
