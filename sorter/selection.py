2f50aece7684ac117ee3f82d4a2a12a8dbbc2d30
James Schinwald <jamesschinwald@gmail.com>
2018-07-25 12:03:37 -0400

    adding selection sort

--- sorter/selection.py
+++ sorter/selection.py
@@ -4,7 +4,19 @@
 # Author: James Schinwald
 
 def sort(array):
-    pass
+    for i in range(0, len(array)):
+        lowest = i
+
+        for k in range(i+1, len(array)):
+            if array[k] < array[lowest]:
+                lowest = k
+
+        swap(array, i, lowest)
+
+def swap(array, a, b):
+    temp = array[a]
+    array[a] = array[b]
+    array[b] = temp
 
 def explain():
     pass
