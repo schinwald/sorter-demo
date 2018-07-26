3a41d1f63ad77ad7f129729529dc3d4df8f57925
James Schinwald <jamesschinwald@gmail.com>
2018-07-26 16:31:24 -0400

    adding comments and description

--- sorter/selection.py
+++ sorter/selection.py
@@ -1,22 +1,37 @@
- # Description:
-# 
+# Description:
+# Using a nested loop, this selects each element in the array and loops through all elements to the right, finding the smallest number and replacing it with the 
+# selected element.
 # 
 # Author: James Schinwald
 
+# sorts the array
 def sort(array):
+    # select an index
     for i in range(0, len(array)):
         lowest = i
 
+        # loop through all elements to the right of the selected index
+        # finding the smallest number
         for k in range(i+1, len(array)):
             if array[k] < array[lowest]:
                 lowest = k
 
+        # swap smallest number with selected indexed number
         swap(array, i, lowest)
 
+# swaps to elements in an array
 def swap(array, a, b):
     temp = array[a]
     array[a] = array[b]
     array[b] = temp
 
+# explanation of the sorting algorithm used
 def explain():
-    pass
+    print("\n   Selection Sort:\n")
+    print("      Using a nested loop, this selects each element in the array and loops through all elements to the right, finding the smallest number and replacing it with the")
+    print("      selected element.\n")
+    print("         Steps:\n")
+    print("            1. Select the first index in the array and mark it as the smallest number.")
+    print("            2. Loop through the rest of the array and select the index of the smallest number, replacing all previous smallest numbers.")
+    print("            3. Swap the selected index with the index of the smallest number.")
+    print("            4. Increment the selected index by one and repeat steps 2-3 until selected index is equal to the last index of the array.\n")
