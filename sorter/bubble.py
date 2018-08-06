ae6569ec557e1c54b7fe838be538773d34bf2128
James Schinwald <jamesschinwald@gmail.com>
2018-08-06 19:25:44 -0400

    adding bubble sort

--- sorter/bubble.py
+++ sorter/bubble.py
@@ -4,7 +4,16 @@
 
 # sorts the array
 def sort(array):
-    pass
+    for i in range(len(array)-1, 0, -1):
+        for j in range(0, i):
+            if array[j] > array[j+1]:
+                swap(array, j, j+1)
+                
+# swaps to elements in an array
+def swap(array, a, b):
+    temp = array[a]
+    array[a] = array[b]
+    array[b] = temp
 
 # explanation of the sorting algorithm used
 def explain():
