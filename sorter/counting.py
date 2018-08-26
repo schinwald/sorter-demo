3d8360bae30ac4b3cacb5bca5d13b15f00e72753
James Schinwald <jamesschinwald@gmail.com>
2018-08-25 23:38:08 -0400

    adding counting sort

--- sorter/counting.py
+++ sorter/counting.py
@@ -4,7 +4,27 @@
 
 # sorts the array
 def sort(array):
-    pass
+    count = []
+    output = [0 for i in range(len(array))]
+
+    for i in range(0, len(array)):
+        index = array[i]
+
+        while index >= len(count):
+            count.append(0)
+
+        count[index] += 1
+
+    for i in range(1, len(count)):
+        count[i] += count[i-1]
+
+    for i in range(0, len(array)):
+        index = count[array[i]]-1
+        count[array[i]] -= 1
+        output[index] = array[i]
+
+    for i in range(0, len(array)):
+        array[i] = output[i]
 
 # explanation of the sorting algorithm used
 def explain():
